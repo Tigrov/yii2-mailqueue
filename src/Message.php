430cb9463ba1bdc750061a539921d73e4f160260
Dmitriy Gritsenko <evil1@inbox.ru>
2024-10-07 11:58:31 +0500

    New dependency library

--- src/Message.php
+++ src/Message.php
@@ -51,30 +51,31 @@ class Message extends \yii\symfonymailer\Message implements MessageInterface
         $this->_model = $model;
         $embedIds = [];
 
-        try {
-            foreach ($model->getData() as $name => $params) {
-                if (in_array($name, self::MULTIPLE_VALUES)) {
-                    foreach ($params as $value) {
-                        if (in_array($name, self::BASE_ENCODED_VALUES) && isset($value[0])) {
-                            $value[0] = base64_decode($value[0]);
-                        }
-                        call_user_func_array(parent::class . '::' . $name, $value);
+        foreach ($model->getData() as $name => $params) {
+            if (in_array($name, self::MULTIPLE_VALUES)) {
+                foreach ($params as $value) {
+                    if (empty($value)) {
+                        continue;
                     }
-                } elseif (in_array($name, self::EMBED_VALUES)) {
-                    foreach ($params as list($content, $options, $id)) {
-                        if (in_array($name, self::BASE_ENCODED_VALUES)) {
-                            $content = base64_decode($content);
-                        }
-                        $embedIds[$id] = call_user_func_array(parent::class . '::' . $name, [$content, $options]);
+                    if (in_array($name, self::BASE_ENCODED_VALUES) && isset($value[0])) {
+                        $value[0] = base64_decode($value[0]);
                     }
-                } else {
-                    if ($name == 'setHtmlBody' && !empty($embedIds)) {
-                        $params[0] = strtr($params[0], $embedIds);
-                    }
-                    call_user_func_array(parent::class . '::' . $name, $params);
+                    call_user_func_array(parent::class . '::' . $name, $value);
                 }
+            } elseif (in_array($name, self::EMBED_VALUES)) {
+                foreach ($params as list($content, $options, $id)) {
+                    if (in_array($name, self::BASE_ENCODED_VALUES)) {
+                        $content = base64_decode($content);
+                    }
+                    $embedIds[$id] = call_user_func_array(parent::class . '::' .  $name, [$content, $options]);
+                }
+            } else {
+                if ($name == 'setHtmlBody' && !empty($embedIds)) {
+                    $params[0] = strtr($params[0], $embedIds);
+                }
+                call_user_func_array(parent::class . '::' . $name, $params);
             }
-        } catch (\Exception $e) {}
+        }
 
         return $this;
     }
